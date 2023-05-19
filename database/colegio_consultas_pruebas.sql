5c29d53a7df3dc9bd90430fd661615f878c50daf
Kado12 <gonzalosh01@gmail.com>
2023-05-18 20:21:29 -0500

    Añadiendo funcion de unidades

--- database/colegio_consultas_pruebas.sql
+++ database/colegio_consultas_pruebas.sql
@@ -5,6 +5,7 @@ select * from evaluacion;
 select * from asistencia;
 select * from sesion;
 select * from estudiante;
+select * from horario;
 
 -- Lista general de todas las Sesiones
 select asi_desc, crs_grado, crs_seccion, uni_titulo, ses_titulo from sesion
@@ -35,6 +36,26 @@ inner join curso on curso.crs_id = curso_profesor.crs_id
 inner join asignatura on asignatura.asi_id = curso_profesor.asi_id
 where eva_tipo = 1 and curso_profesor.crs_id =1;
 
+/* Actualizar horario
+UPDATE horario
+SET hor_hora_inicio = CASE 
+                    WHEN hor_id = 4 THEN '10:30:00'
+                    WHEN hor_id = 5 THEN '11:15:00'
+                    WHEN hor_id = 6 THEN '12:00:00'
+                    WHEN hor_id = 7 THEN '12:45:00'
+                    ELSE hor_hora_inicio
+                END,
+    hor_hora_fin = CASE 
+                    WHEN hor_id = 4 THEN '11:15:00'
+                    WHEN hor_id = 5 THEN '12:00:00'
+                    WHEN hor_id = 6 THEN '12:45:00'
+                    WHEN hor_id = 7 THEN '13:30:00'
+                    ELSE hor_hora_fin
+                END
+WHERE hor_id IN (4,5,6,7);
+*/
+
+
 
 /* TESTEO DE PROCEDIMIENTOS ALMACENADOS */
 use db_colegio;
@@ -59,3 +80,81 @@ CALL SP_unidades(1); /* ID de Aula-Profesor */
 
 -- Sesión Contenido
 CALL SP_sesion_contenido(1); /* ID de la Sesión */
+
+-- Evaluaciones
+CALL SP_evaluaciones(1); /* ID de Aula-Profesor */
+
+-- Notas de Estudiante
+CALL SP_notas_estudiante('45678912', 1); /* ID de Estudiante e ID de Aula-Profesor */
+
+-- aulas de un profesor
+CALL SP_aulas('45679212'); /* ID de Profesor */
+
+-- Lista de estudiantes de un Aula y una Asignatura
+CALL SP_lista_estudiantes_aula(1); /* ID de Aula-Profesor */
+
+-- Información de fecha con respecto a una sesión
+CALL SP_informacion_fecha('2023-04-03'); /* La fecha actual */
+
+-- Lista de Unidades de un Aula
+CALL SP_lista_unidades_aula(1); /* ID de Aula-Profesor */
+
+-- Lista de Sesiones de una Unidad
+CALL SP_lista_sesiones_unidad(1); /* ID de la Unidad */
+
+-- Lista de Fechas de una Sesión
+CALL SP_lista_fechas_sesion(1); /* ID de la Sesión */
+
+-- Agregar Nueva Fecha
+CALL SP_agregar_fecha(1, '2023-05-16'); /* ID de la Sesión y nueva fecha */
+
+-- Agregar falta de estudiantes
+CALL SP_agregar_falta(1, '45678912,45678913,45678917'); /* ID de fecha-sesion y una cadena de ID'S de Estudiantes separados por coma */
+
+-- Editar Sesion
+CALL SP_editar_sesion(1, 'Sesión 1'); /* ID y nuevo título de la sesión */
+
+-- Editar Evaluación
+CALL SP_editar_evaluacion(1, 'Problemas de Matemáticas', 'Resolver problemas Matemáticos que involucren conceptos como álgebra, geometría, trigonometría, etc.' , '2023-04-03 08:10:00', '2023-04-10 10:10:00', 'www.colegio/tareas/JJJ',1); /* ID, titulo, descripcion, fecha inicio, fecha fin, link, tipo evaluacion */
+
+-- Información Evaluación
+CALL SP_informacion_evaluacion(1); /* ID de la evaluación */
+
+-- Guardar Calificación
+CALL SP_guardar_calificacion(19,'45678912',1); /* Calificación, ID del estudiante e ID de la evaluación */
+
+-- Agregar Nueva Sesión
+CALL SP_agregar_sesion(221,'Sesión 4'); /* ID de la Unidad y título de la nueva sesión */
+
+-- Agregar Nueva Clase
+CALL SP_agregar_clase('Diapositivas','www.colegio/archivos/ASD', 1); /* Titulo, link de la Nueva Clase (archivos) e ID de la Sesión selecionada */
+
+-- Agregar Nueva Evaluación con Sesión existente
+CALL SP_agregar_evaluacion_sesionExiste('Problemas de Integrales', 'Resolver 5 problemas de integrales', '2023-04-03 08:20:00', '2023-04-10 10:20:00', 'www.colegio/tareas/HHH', 1, 1); /* Titulo, Desripcion, Fecha de Inicio, Fecha Final, Link, Tipo de la nueva evaluación y el ID de la sesión seleccionada */
+
+-- Agregar Nueva Evaluación con Sesión NO existente
+CALL SP_agregar_evaluacion_sesionNoExiste( 1,'Sesión 6', 'Problemas de Integrales 3', 'Resolver 5 problemas de integrales', '2023-04-03 08:20:00', '2023-04-10 10:20:00', 'www.colegio/tareas/HHH', 1,1); /* ID de la Unidad, Titulo de la nueva Sesión, Titulo, Desripcion, Fecha de Inicio, Fecha Final, Link, Tipo de la nueva evaluación y el ID de la sesión seleccionada */
+
+-- Notas de estudiantes de un Aula
+select ROW_NUMBER() OVER (ORDER BY evaluacion.eva_id and usr_apellidos) AS 'N°', CONCAT(usr_apellidos, ' ' ,usr_nombres) as 'ESTUDIANTE', not_calificacion as 'NOTA', nota_alumno.eva_id as 'ID EVALUACIÓN'
+from nota_alumno
+left join estudiante on estudiante.est_usr_id = nota_alumno.est_usr_id
+inner join usuario on usuario.usr_id = estudiante.est_usr_id
+inner join evaluacion on evaluacion.eva_id = nota_alumno.eva_id
+where estudiante.est_usr_id 
+IN (
+	select estudiante.est_usr_id
+	from estudiante
+	inner join curso on curso.crs_id = estudiante.crs_id
+	inner join curso_profesor on curso_profesor.crs_id = curso.crs_id
+	where curso_profesor.curpro_id = 1
+)
+AND nota_alumno.eva_id
+IN (
+	select evaluacion.eva_id
+    from evaluacion
+	inner join sesion on sesion.ses_id = evaluacion.ses_id
+	inner join unidad on unidad.uni_id = sesion.uni_id
+	inner join curso_profesor on curso_profesor.curpro_id = unidad.curpro_id
+	where curso_profesor.curpro_id = 1
+) order by evaluacion.eva_id and usr_apellidos;
--- main.js
+++ main.js
@@ -24,8 +24,14 @@ app.post('/auth', async (req, res)=>{
     const user = req.body.user
     const pass = req.body.pass
     if(user && pass){
-        connection.query("SELECT * FROM usuario WHERE usr_id = ?", [user], async (err, results)=>{
-            if (results.length == 0 || !(await pass == results[0].usr_pass)){
+        connection.query('CALL SP_datos_usuario(?)', [user], async (err, results)=>{
+            // Guardando los datos de usuario al iniciar sesión
+            req.session.cod = results[0][0].ID
+            req.session.pass = results[0][0].CONTRA
+            req.session.name =results[0][0].NOMBRES +' '+ results[0][0].APELLIDOS
+            req.session.type = results[0][0]['ID TIPO']
+            // validando datos de sesión
+            if (results.length == 0 || !(await pass == req.session.pass)){
                 res.render('index',{
                     alert: true,
                     alertTitle: "Error",
@@ -36,22 +42,17 @@ app.post('/auth', async (req, res)=>{
                     ruta: ''
                 })
             } else {
-                const tipo = results[0].tip_id
-                if(tipo == 1){
+                if(req.session.type == 1){
                     ruta = 'maine'
-                }
-                if(tipo == 2){
+                    req.session.grado = results[0][0].GRADO
+                    req.session.seccion = results[0][0]['SECCIÓN']
+                    req.session.idaula = results[0][0]['ID AULA']
+                } else if(req.session.type == 2){
                     ruta = 'mainp'
-                }
-                if(tipo == 3){
+                } else{
                     ruta = 'maind'
                 }
-                req.session.ruta = 'maine'
-                req.session.type = results[0].tip_id
                 req.session.loggedin = true
-                req.session.cod = results[0].usr_id
-                //console.log(req.session.cod)
-                req.session.name =results[0].usr_nombres +' '+ results[0].usr_apellidos
                 res.render('index', {
                     alert: true,
                     alertTitle: "Conexion exitosa",
@@ -67,7 +68,7 @@ app.post('/auth', async (req, res)=>{
         res.render('index', {
             alert: true,
             alertTitle: "Advertencia",
-            alertMessage: "Coloca tus datos pe sonso 👊🏼",
+            alertMessage: "Coloca tus datos",
             alertIcon: "warning",
             showConfirmButton: true,
             timer: false,
@@ -78,11 +79,12 @@ app.post('/auth', async (req, res)=>{
 // Verificación de sesión de cada página
 app.get('/maine', (req, res) => {
     if(req.session.loggedin && req.session.type == 1){
-        connection.query('SELECT * FROM asignatura', (err, asig) =>{
+        connection.query('CALL SP_cursos_estudiante(?)', [req.session.idaula], (err, results) => {
+            req.session.cursos = results[0]
             res.render('index-e',{
                 login: true,
                 name: req.session.name,
-                data: asig
+                data: req.session.cursos
             })
         })
     }else{
@@ -123,60 +125,55 @@ app.post('/asignatura', async (req, res) => {
     const botonSeleccionado = Object.keys(req.body).find(key => key.startsWith('asignatura_'))
     const idAsignatura = botonSeleccionado.replace('asignatura_', '')
     console.log('Se ha seleccionado el botón con ID de asignatura:', idAsignatura)
-    const getAsignatura = () => {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT asignatura.asi_id, asignatura.asi_desc, profesor.pro_usr_id, usuario.usr_nombres, usuario.usr_apellidos,curso.crs_id,curso.crs_grado,curso.crs_seccion,curso_horario.curhor_dia,horario.hor_hora_inicio,horario.hor_hora_fin FROM asignatura INNER JOIN curso_profesor ON asignatura.asi_id = curso_profesor.asi_id INNER JOIN curso ON curso_profesor.crs_id = curso.crs_id INNER JOIN curso_horario ON curso_profesor.curpro_id = curso_horario.curpro_id INNER JOIN horario ON curso_horario.hor_id = horario.hor_id INNER JOIN profesor ON curso_profesor.pro_usr_id = profesor.pro_usr_id INNER JOIN usuario ON profesor.pro_usr_id = usuario.usr_id WHERE asi_desc = ?', [idAsignatura], (err, result) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    req.session.hordia = result[0].curhor_dia
-                    req.session.nomasig = result[0].asi_desc
-                    req.session.prof = result[0].usr_nombres + ' ' + result[0].usr_apellidos
-                    req.session.horario = result[0].hor_hora_inicio + '-' + result[0].hor_hora_fin
-                    resolve(result)
-                }
-            });
-        });
-    };
-    const getEstudiante = () => {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT usuario.usr_id, estudiante.crs_id, curso.crs_grado, curso.crs_seccion FROM usuario INNER JOIN estudiante ON usuario.usr_id = estudiante.est_usr_id INNER JOIN curso ON estudiante.crs_id = curso.crs_id WHERE usr_id = ?', [req.session.cod], (err, result) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    req.session.seccion = result[0].crs_seccion;
-                    resolve(result)
-                }
-            });
-        });
-    };
-    try {
-        const [asignatura, estudiante] = await Promise.all([getAsignatura(), getEstudiante()])
+    connection.query('CALL SP_obtener_informacion_curso(?)', [parseInt(idAsignatura)], (err, results) => {
+        console.log(results[0])
+        req.session.curprof = parseInt(idAsignatura)
+        req.session.profesor = results[0][0].PROFESOR
+        req.session.descurso = results[0][0].DESCRIPCION_ASIGNATURA
+        req.session.diashor = results[0]
         console.log('Información obtenida exitosamente.')
         res.redirect('/asig-estu')
-    } catch (error) {
-        console.error(error)
-        res.status(500).send('Error al obtener información.')
-    }
+    })
 })
-// Renderizar pagina de asignatura
+// Renderizar página de asignatura
 app.get('/asig-estu', (req,res) => {
     if(req.session.loggedin && req.session.type == 1){
         res.render('e-asig',{
             login: true,
             name: req.session.name,
-            nomasig: req.session.nomasig,
-            prof: req.session.prof,
-            horario: req.session.horario,
-            hordia: req.session.hordia,
+            nomasig: req.session.descurso,
+            prof: req.session.profesor,
+            hotdia: req.session.diashor,
             seccion: req.session.seccion
         })
     }else{
-        res.render('index-e',{
+        res.render('e-asig',{
             login: false,
             name: 'Debe iniciar sessión'
         })
     }
+})
+// Verificación de acción seleccionada por Estudiante
+app.post('/accion-estudiante', async (req, res) => {
+    const botonSeleccionado = Object.keys(req.body)
+    console.log('Se ha seleccionado la accion', botonSeleccionado.join(''))
+    connection.query('CALL SP_obtener_datos(?)', [req.session.curprof], (err, results) => {
+        console.log(results)
+        req.session.archivos = results[0]
+        req.session.evaluaciones = results[1]
+        console.log(req.session.archivos.length)
+        console.log(req.session.evaluaciones.length)
+        res.redirect('/action-estu')
+    })  
+})
+// Renderizar página de accion
+app.get('/action-estu', (req,res) => {
+    res.render('e-unidades', {
+        name: req.session.name,
+        archivos: req.session.archivos,
+        evaluaciones: req.session.evaluaciones,
+        login: true
+    })
 })
 // Cerrar sesión
 app.get('/logout', (req, res)=>{
